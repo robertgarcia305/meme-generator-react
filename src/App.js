f2721df9480a06b7bf84f5c0a2797c0737a490d4
Robert Garcia <robertserranogar@gmail.com>
2023-08-16 20:40:50 -0600

    added react files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+// steps:
+//go to your directory where you want to add the react app
+//   my-app should be changed to whatever your name of the app
+//   npx create-react-app my-app
+//   cd my-app
+
+//   npm start
+// if npm start doesn't work do: 
+
+// npm install react-scripts
+// then react-scripts start
+
 import logo from './logo.svg';
 import './App.css';
 
