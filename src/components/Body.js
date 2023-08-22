f0728e94555afa37f9c5b9084bc4290cfccbd13d
Robert Garcia <77760521+robertgarcia305@users.noreply.github.com>
2023-08-22 14:53:27 -0600

    Add files via upload

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,10 +13,49 @@ const Body = (props) => {
                     <button onClick={props.function}>Generate Image</button>
                 </div>
             </div>
-            <div className="meme-img" >
+            {/* HERE ADD THE TEXTS AS PROPS */}
+            <div class="image-wrap">
                 <img className="memeImg" src={props.image}></img>
+                <div class="text-wrap">
+                    <p className="top-text">{props.top}</p>
+                    <p className="bottom-text">{props.bottom}</p>
+                </div>
             </div>
         </section>
+
+// .container {
+//     border: 1px solid red;
+//     position: alternative;
+//     display: flex;
+//     height: 200px;
+//     justify-content: center;
+//   }
+  
+//   img {
+//     height: 200px;
+//     position: absolute;
+//   }
+  
+//   .text-wrap {
+//     border: 1px solid blue;
+//     height: auto;
+//     z-index: 2;
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     justify-content: space-between;
+//   }
+  
+//   .text-wrap p {
+//     background-color: black;
+//     font-family: sans-serif;
+//     color: white;
+//     padding: 7px;
+//     border-radius: 10px;
+//     margin: 3px 0px;
+//   }
+  
+  
     );
 }
 
